ddba9410438a40aec626eca770b8a997eeb05499
MihneaTeodorStoica <ms7322@columbia.edu>
2025-09-21 17:18:05 +0300

    Remade the DEBUG module.

--- main.cpp
+++ main.cpp
@@ -8,54 +8,123 @@
 */
 #pragma GCC optimize("Ofast,unroll-loops,inline-functions")
 #include <bits/stdc++.h>
-#include <bits/extc++.h>
 #pragma GCC target("sse4.2,avx2,bmi2,fma,popcnt,arch=skylake,tune=native")
 using namespace std;
-using namespace __gnu_pbds;
-template<typename... Args>void debug(const Args&...);
-// ===============================================================
-/**
- * Problem name: ${title}
- * URL: ${url}
- * Time Limit: ${timeLimit} ms
- * 
- * Good Luck!
- */
-int TC = 1, TT = 1;
-inline void tc() {
-    
-}
+
+// ============================ toggles ============================
 #define MULTIPLE_TESTCASES 1
+#define DEFINE_INT_LL      1
 #define DEBUG              0
 #define FASTIO             1
 #define FILEIO             0
 #define FILENAME           ""
-// ===============================================================
-#if DEBUG
-template<typename T> void _print_one(const T& x) { cout << x; }
-template<typename T, typename... Args> void _print_one(const T& first, const Args&... rest) { cout << first; _print_one(rest...); }
-template<typename... Args> void debug(const Args&... args) { _print_one(args...); cout << "\n"; }
+
+// feature toggles
+#define USE_RNG            1
+
+// ============================ base types =========================
+#if DEFINE_INT_LL
+#  define int long long
+using uint = unsigned long long;
 #else
-template<typename... Args> void debug(const Args&...) {}
+using uint = unsigned int;
 #endif
-signed main() {
+using ll = long long; using ull = unsigned long long; using ld = long double;
+
+// ============================== debug ============================
+// Zero-overhead when DEBUG==0. Pretty-prints scalars, pairs, tuples, ranges.
+#if DEBUG
+namespace dbg {
+    using std::cerr; using std::string; using std::string_view;
+
+    template<class T, class = void> struct is_iterable : std::false_type {};
+    template<class T> struct is_iterable<T, std::void_t<decltype(begin(std::declval<T>())), decltype(end(std::declval<T>()))>> : std::true_type {};
+    template<class T> inline constexpr bool is_stringlike =
+        std::is_same_v<std::decay_t<T>, string> || std::is_same_v<std::decay_t<T>, string_view> || std::is_same_v<std::decay_t<T>, const char*>;
+
+    inline void _print(const string& s){ cerr<<s; } inline void _print(string_view s){ cerr<<s; }
+    inline void _print(const char* s){ cerr<<s; } inline void _print(char c){ cerr<<c; }
+    inline void _print(bool b){ cerr<<(b?"true":"false"); }
+    template<class T> std::enable_if_t<!is_iterable<T>::value && !is_stringlike<T>> _print(const T& x){ cerr<<x; }
+
+    template<class A, class B> void _print(const pair<A,B>& p){ cerr<<'(' ; _print(p.first); cerr<<','; _print(p.second); cerr<<')'; }
+    template<class... Ts> void _print(const tuple<Ts...>& t){
+        cerr<<'(';
+        size_t k=0; std::apply([&](auto const&... xs){ ((k++?cerr<<',' : cerr, _print(xs)), ...); }, t);
+        cerr<<')';
+    }
+    template<class T> std::enable_if_t<is_iterable<T>::value && !is_stringlike<T>> _print(const T& a){
+        cerr<<'['; bool f=true; for (auto const& v: a){ if(!f) cerr<<','; f=false; _print(v);} cerr<<']';
+    }
+
+    inline void _split_names(string_view s, vector<string_view>& out){
+        size_t i=0, n=s.size(), par=0, brk=0, ang=0;
+        auto flush=[&](size_t l,size_t r){ while(l<r && s[l]==' ') ++l; while(r>l && s[r-1]==' ') --r; if(l<r) out.emplace_back(s.substr(l,r-l)); };
+        for(size_t j=0;j<n;++j){ char c=s[j];
+            par += (c=='(') - (c==')'); brk += (c=='[') - (c==']'); ang += (c=='<') - (c=='>');
+            if(c==',' && !par && !brk && !ang){ flush(i,j); i=j+1; }
+        } flush(i,n);
+    }
+    template<class... Args> void vars(string_view names, const Args&... args){
+        vector<string_view> v; v.reserve(sizeof...(Args)); _split_names(names, v); size_t i=0;
+        auto one=[&](auto const& val){ if(i<v.size()) { cerr<<v[i]<<'='; } _print(val); if(++i<v.size()) cerr<<" | "; };
+        (one(args), ...); cerr<<'\n';
+    }
+}
+#  define DBG(...) do{ std::cerr<<"[L"<<__LINE__<<"] "; dbg::vars(#__VA_ARGS__, __VA_ARGS__); }while(0)
+#  define DUMP(expr) do{ std::cerr<<"[L"<<__LINE__<<"] "<<#expr<<'='; dbg::_print((expr)); std::cerr<<'\n'; }while(0)
+#  define ASSERT(x) do{ if(!(x)){ std::cerr<<"ASSERT FAIL @L"<<__LINE__<<": " #x "\n"; std::abort(); } }while(0)
+#else
+#  define DBG(...)   do{}while(0)
+#  define DUMP(x)    do{}while(0)
+#  define ASSERT(x)  do{ (void)sizeof(x); }while(0)
+#endif
+
+// ============================== rng ==============================
+#if USE_RNG
+static uint64_t splitmix64(uint64_t x){ x+=0x9e3779b97f4a7c15ULL; x=(x^(x>>30))*0xbf58476d1ce4e5b9ULL; x=(x^(x>>27))*0x94d049bb133111ebULL; return x^(x>>31); }
+struct FastRNG{
+    uint64_t s; FastRNG(): s(chrono::high_resolution_clock::now().time_since_epoch().count()) {}
+    uint64_t operator()(){ return s = splitmix64(s); }
+    uint64_t operator()(uint64_t lim){ return operator()()%lim; }
+    template<class It> void shuffle(It l, It r){ for(auto n=r-l; n>1; --n){ auto j=(*this)(n); iter_swap(l+(n-1), l+j); } }
+} rng;
+#endif
+
+// ============================ problem glue =======================
+/**
+ * Problem: ${title}
+ * URL:    ${url}
+ * TL:     ${timeLimit} ms
+ */
+signed TC = 1, TT = 1;
+
+inline void tc() {
+    
+}
+
+signed main(){
 #if FASTIO
-    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ios::sync_with_stdio(false); cin.tie(nullptr);
 #endif
 #if FILEIO
-    #if FILENAME[0] != '\0'
+  #if FILENAME[0] != '\0'
     freopen(FILENAME ".in", "r", stdin);
     freopen(FILENAME ".out", "w", stdout);
-    #else
-        #error "FILEIO enabled but FILENAME is empty"
-    #endif
+  #else
+    #error "FILEIO enabled but FILENAME is empty"
+  #endif
 #endif
+
 #if MULTIPLE_TESTCASES
     cin >> TT;
+#else
+    TT = 1;
 #endif
-    for (; TC <= TT; TC ++) {
+
+    for (; TC <= TT; ++TC){
 #if DEBUG
-        cout << "Testcase #" << TC << ":\n";
+        cerr << "Testcase#" << TC << '\n';
 #endif
         tc();
     }
